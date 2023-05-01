ab919b1d3f91e2510ccf318420a248766fbfd8be
Mariia <masha239@gmail.com>
2023-05-01 15:41:30 +0600

    format_dataset added

--- utils.py
+++ utils.py
@@ -1,17 +1,33 @@
-import random
+def format_dataset(sample):
+    question = sample['question']['text']
+    context = sample['document']['tokens']['token']
+    is_html = sample['document']['tokens']['is_html']
+    long_answers = sample['annotations']['long_answer']
+    short_answers = sample['annotations']['short_answers']
 
+    context_string = " ".join([context[i] for i in range(len(context)) if not is_html[i]])
 
-def sample_jsonl_dataset(dataset_path, sampled_dataset_path, n_sample):
-    with open(dataset_path, 'r') as json_file:
-        json_list = list(json_file)
-    sample = random.sample(json_list, n_sample)
-    with open(sampled_dataset_path, 'w') as f:
-        for item in sample:
-            f.write(item)
+    for answer in sample['annotations']['yes_no_answer']:
+        if answer == 0 or answer == 1:
+            return {"question": question, "context": context_string, "short": [], "long": [],
+                    "category": "no" if answer == 0 else "yes"}
 
+    short_targets = []
+    for s in short_answers:
+        short_targets.extend(s['text'])
+    short_targets = list(set(short_targets))
 
-if __name__ == '__main__':
-    n_sample = 50000
-    dataset_path = '/users/masha239/Downloads/v1.0-simplified_simplified-nq-train.jsonl'
-    sampled_dataset_path = '/users/masha239/Downloads/data_sampled.jsonl'
-    sample_jsonl_dataset(dataset_path, sampled_dataset_path, n_sample)
+    long_targets = []
+    for s in long_answers:
+        if s['start_token'] == -1:
+            continue
+        answer = context[s['start_token']: s['end_token']]
+        html = is_html[s['start_token']: s['end_token']]
+        new_answer = " ".join([answer[i] for i in range(len(answer)) if not html[i]])
+        if new_answer not in long_targets:
+            long_targets.append(new_answer)
+
+    category = "long_short" if len(short_targets + long_targets) > 0 else "null"
+
+    return {"question": question, "context": context_string, "short": short_targets, "long": long_targets,
+            "category": category}
